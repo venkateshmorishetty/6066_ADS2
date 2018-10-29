f0204fe8c3edeb47bf691cf88f4af04077e5ba6b
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-29 22:19:06 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m1/1.1 Graph/Bag.java
+++ m1/1.1 Graph/Bag.java
@@ -1,35 +1,32 @@
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
-
-    // helper linked list class
+    /**
+     * { N }.
+     */
+    private int N;
+    /**
+     * { first }.
+     */
+    private Node first;
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**.
+         * { item }
+         */
         private Item item;
+        /**.
+         * { next node address }
+         */
         private Node next;
     }
-
    /**
      * Create an empty stack.
      */
@@ -37,16 +34,18 @@ public class Bag<Item> implements Iterable<Item> {
         first = null;
         N = 0;
     }
-
-   /**
-     * Is the BAG empty?
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
-
-   /**
-     * Return the number of items in the bag.
+    /**
+     * { size function }.
+     *
+     * @return     { size }
      */
     public int size() {
         return N;
@@ -55,33 +54,41 @@ public class Bag<Item> implements Iterable<Item> {
    /**
      * Add the item to the bag.
      */
-    public void add(Item item) {
+    public void add(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
         N++;
     }
-
-
-   /**
-     * Return an iterator that iterates over the items in the bag.
+    /**.
+     * { iterator }
+     *
+     * @return     { list }
      */
     public Iterator<Item> iterator()  {
         return new ListIterator();  
     }
-
-    // an iterator, doesn't implement remove() since it's optional
+    /**.
+     * Class for list iterator.
+     */
     private class ListIterator implements Iterator<Item> {
+        /**
+         * { current node }.
+         */
         private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        public boolean hasNext()  { 
+            return current != null; 
+        }
+        public void remove() {
+            throw new UnsupportedOperationException(); 
+        }
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
--- m1/1.1 Graph/Solution.java
+++ m1/1.1 Graph/Solution.java
@@ -1,302 +1,302 @@
 import java.util.Scanner;
 interface Graph {
-	/**
-	 * { vertices }
-	 *
-	 * @return     { number of edges }
-	 */
-	int vertices();
-	/**
-	 * { edges }
-	 *
-	 * @return     { number of edges }
-	 */
-	int edges();
-	/**
-	 * Adds an edge.
-	 *
-	 * @param      v     { vertex v }
-	 * @param      w     { vertex w }
-	 */
-	void addEdge(int v, int w);
-	/**
-	 * { adj function }
-	 *
-	 * @param      v     { vertex v }
-	 *
-	 * @return     {vertices adjecent to v} }
-	 */
-	Iterable<Integer> adj(int v);
-	/**
-	 * Determines if it has edge.
-	 *
-	 * @param      v     { vertex v }
-	 * @param      w     { vertex e }
-	 *
-	 * @return     True if has edge, False otherwise.
-	 */
-	boolean hasEdge(int v, int w);
+    /**.
+     * { vertices }
+     *
+     * @return     { number of edges }
+     */
+    int vertices();
+    /**.
+     * { edges }
+     *
+     * @return     { number of edges }
+     */
+    int edges();
+    /**.
+     * Adds an edge.
+     *
+     * @param      v     { vertex v }
+     * @param      w     { vertex w }
+     */
+    void addEdge(int v, int w);
+    /**.
+     * { adj function }
+     *
+     * @param      v     { vertex v }
+     *
+     * @return     {vertices adjecent to v} }
+     */
+    Iterable<Integer> adj(int v);
+    /**.
+     * Determines if it has edge.
+     *
+     * @param      v     { vertex v }
+     * @param      w     { vertex e }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    boolean hasEdge(int v, int w);
 }
 /**
  * List of adjacencies.
  */
 class AdjacencyList implements Graph {
-	/**
-	 * { number of vertices }.
-	 */
-	private int nodes;
-	/**
-	 * { number of edges }.
-	 */
-	private int edges;
-	/**
-	 * { count of valid edges }.
-	 */
-	private int count = 0;
-	/**
-	 * { bag array }.
-	 */
-	private Bag<Integer>[] list;
-	/**
-	 * { keys }.
-	 */
-	private String[] keys;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      n     { number of vertices }
-	 * @param      e     { number of edges }
-	 * @param      k     { keys }
-	 */
-	AdjacencyList(final int n, final int e, final String[] k) {
-		nodes = n;
-		edges = e;
-		list = new Bag[n];
-		for (int i = 0; i < n; i++) {
-			list[i] = new Bag();
-		}
-		keys = k;
-	}
-	/**
-	 * { total vertices }.
-	 *
-	 * @return     { count of vertices }
-	 */
-	public int vertices() {
-		return nodes;
-	}
-	/**.
-	 * { edges }
-	 *
-	 * @return     { count of edges }
-	 */
-	public int edges() {
-		return edges;
-	}
-	/**
-	 * Adds an edge.
-	 *
-	 * @param      v1    The v 1
-	 * @param      v2    The v 2
-	 */
-	public void addEdge(final int v1, final int v2) {
-		if (!hasEdge(v1, v2)) {
-			count++;
-		}
-		list[v1].add(v2);
-		list[v2].add(v1);
-	}
-	/**
-	 * Determines if it has edge.
-	 *
-	 * @param      v1    The v 1
-	 * @param      v2    The v 2
-	 *
-	 * @return     True if has edge, False otherwise.
-	 */
-	public boolean hasEdge(final int v1, final int v2) {
-		for (int k : adj(v1)) {
-			if (v2 == k) {
-				return true;
-			}
-		}
-		return false;
-	}
-	/**
-	 * { adjecents connected to vertex }.
-	 *
-	 * @param      v     { vertex }
-	 *
-	 * @return     { return adjecent vertices for given vertex }
-	 */
-	public Iterable<Integer> adj(final int v) {
-		return list[v];
-	}
-	/**.
-	 * { print the graph }
-	 */
-	public void print() {
-		System.out.println(nodes + " vertices"
-			+ ", " + count + " edges");
-		if (count == 0) {
-			System.out.println("No edges");
-			return;
-		}
-		for (int i  = 0; i < nodes; i++) {
-			System.out.print(keys[i] + ": ");
-			for (int k : adj(i)) {
-				System.out.print(keys[k] + " ");
-			} System.out.println();
-		}
-	}
+    /**
+     * { number of vertices }.
+     */
+    private int nodes;
+    /**
+     * { number of edges }.
+     */
+    private int edges;
+    /**
+     * { count of valid edges }.
+     */
+    private int count = 0;
+    /**
+     * { bag array }.
+     */
+    private Bag<Integer>[] list;
+    /**
+     * { keys }.
+     */
+    private String[] keys;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { number of vertices }
+     * @param      e     { number of edges }
+     * @param      k     { keys }
+     */
+    AdjacencyList(final int n, final int e, final String[] k) {
+        nodes = n;
+        edges = e;
+        list = new Bag[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = new Bag();
+        }
+        keys = k;
+    }
+    /**
+     * { total vertices }.
+     *
+     * @return     { count of vertices }
+     */
+    public int vertices() {
+        return nodes;
+    }
+    /**.
+     * { edges }
+     *
+     * @return     { count of edges }
+     */
+    public int edges() {
+        return edges;
+    }
+    /**
+     * Adds an edge.
+     *
+     * @param      v1    The v 1
+     * @param      v2    The v 2
+     */
+    public void addEdge(final int v1, final int v2) {
+        if (!hasEdge(v1, v2)) {
+            count++;
+        }
+        list[v1].add(v2);
+        list[v2].add(v1);
+    }
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v1    The v 1
+     * @param      v2    The v 2
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v1, final int v2) {
+        for (int k : adj(v1)) {
+            if (v2 == k) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * { adjecents connected to vertex }.
+     *
+     * @param      v     { vertex }
+     *
+     * @return     { return adjecent vertices for given vertex }
+     */
+    public Iterable<Integer> adj(final int v) {
+        return list[v];
+    }
+    /**.
+     * { print the graph }
+     */
+    public void print() {
+        System.out.println(nodes + " vertices"
+            + ", " + count + " edges");
+        if (count == 0) {
+            System.out.println("No edges");
+            return;
+        }
+        for (int i  = 0; i < nodes; i++) {
+            System.out.print(keys[i] + ": ");
+            for (int k : adj(i)) {
+                System.out.print(keys[k] + " ");
+            } System.out.println();
+        }
+    }
 }
 /**
  * Class for adjacency matrix.
  */
 class AdjacencyMatrix implements Graph {
-	/**
-	 * { nodes }.
-	 */
-	private int nodes;
-	/**.
-	 * { edges }
-	 */
-	private int edges;
-	/**
-	 * { count of valid edges }.
-	 */
-	private int count = 0;
-	/**.
-	 * { matrix }
-	 */
-	private int[][] matrix;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      n     { nodes }
-	 * @param      e     { edges }
-	 */
-	AdjacencyMatrix(final int n, final int e) {
-		nodes = n;
-		edges = e;
-		matrix = new int[nodes][nodes];
-	}
-	/**
-	 * { tota number of vertices }.
-	 *
-	 * @return     { count of vertices }
-	 */
-	public int vertices() {
-		return nodes;
-	}
-	/**
-	 * { total number of edges }.
-	 *
-	 * @return     { count of edges }
-	 */
-	public int edges() {
-		return edges;
-	}
-	/**
-	 * { adjecent vertices for given vertex }.
-	 *
-	 * @param      v     { vertex }
-	 *
-	 * @return     { adjecent vertices for given vertex }
-	 */
-	public Iterable<Integer> adj(final int v) {
-		return null;
-	}
-	/**
-	 * Adds an edge.
-	 *
-	 * @param      v1    The v 1
-	 * @param      v2    The v 2
-	 */
-	public void addEdge(final int v1, final int v2) {
-		if (matrix[v1][v2] == 0 && matrix[v2][v1] == 0) {
-			matrix[v1][v2] = 1;
-			matrix[v2][v1] = 1;
-			count++;
-		}
-	}
-	/**
-	 * Determines if it has edge.
-	 *
-	 * @param      v1    The v 1
-	 * @param      v2    The v 2
-	 *
-	 * @return     True if has edge, False otherwise.
-	 */
-	public boolean hasEdge(final int v1, final int v2) {
-		return matrix[v1][v2] == 1;
-	}
-	/**.
-	 * { print the graph }
-	 */
-	public void print() {
-		System.out.println(nodes + " vertices"
-			+ ", " + count + " edges");
-		if (count == 0) {
-			System.out.println("No edges");
-			return;
-		}
-		for (int i = 0; i < nodes; i++) {
-			for (int j = 0; j < nodes; j++) {
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+    /**
+     * { nodes }.
+     */
+    private int nodes;
+    /**.
+     * { edges }
+     */
+    private int edges;
+    /**
+     * { count of valid edges }.
+     */
+    private int count = 0;
+    /**.
+     * { matrix }
+     */
+    private int[][] matrix;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { nodes }
+     * @param      e     { edges }
+     */
+    AdjacencyMatrix(final int n, final int e) {
+        nodes = n;
+        edges = e;
+        matrix = new int[nodes][nodes];
+    }
+    /**
+     * { tota number of vertices }.
+     *
+     * @return     { count of vertices }
+     */
+    public int vertices() {
+        return nodes;
+    }
+    /**
+     * { total number of edges }.
+     *
+     * @return     { count of edges }
+     */
+    public int edges() {
+        return edges;
+    }
+    /**
+     * { adjecent vertices for given vertex }.
+     *
+     * @param      v     { vertex }
+     *
+     * @return     { adjecent vertices for given vertex }
+     */
+    public Iterable<Integer> adj(final int v) {
+        return null;
+    }
+    /**
+     * Adds an edge.
+     *
+     * @param      v1    The v 1
+     * @param      v2    The v 2
+     */
+    public void addEdge(final int v1, final int v2) {
+        if (matrix[v1][v2] == 0 && matrix[v2][v1] == 0) {
+            matrix[v1][v2] = 1;
+            matrix[v2][v1] = 1;
+            count++;
+        }
+    }
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v1    The v 1
+     * @param      v2    The v 2
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v1, final int v2) {
+        return matrix[v1][v2] == 1;
+    }
+    /**.
+     * { print the graph }
+     */
+    public void print() {
+        System.out.println(nodes + " vertices"
+            + ", " + count + " edges");
+        if (count == 0) {
+            System.out.println("No edges");
+            return;
+        }
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
 /**
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//constructor
-	}
-	/**.
-	 * { main function }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String type = sc.nextLine();
-		int nodes = Integer.parseInt(sc.nextLine());
-		int edges = Integer.parseInt(sc.nextLine());
-		String[] keys = sc.nextLine().split(",");
-		switch (type) {
-		case "List":
-			AdjacencyList al = new AdjacencyList(
-				nodes, edges, keys);
-			for (int i = 0; i < edges; i++) {
-				String[] values = sc.nextLine().split(" ");
-				if (!values[0].equals(values[1])) {
-					al.addEdge(Integer.parseInt(values[0]),
-						Integer.parseInt(values[1]));
-				}
-			}
-			al.print();
-			break;
-		case "Matrix":
-			AdjacencyMatrix am = new AdjacencyMatrix(nodes, edges);
-			for (int i = 0; i < edges; i++) {
-				String[] values = sc.nextLine().split(" ");
-				if (!values[0].equals(values[1])) {
-					am.addEdge(Integer.parseInt(values[0]),
-						Integer.parseInt(values[1]));
-				}
-			}
-			am.print();
-			break;
-		default:
-			break;
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor
+    }
+    /**.
+     * { main function }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String type = sc.nextLine();
+        int nodes = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        String[] keys = sc.nextLine().split(",");
+        switch (type) {
+        case "List":
+            AdjacencyList al = new AdjacencyList(
+                nodes, edges, keys);
+            for (int i = 0; i < edges; i++) {
+                String[] values = sc.nextLine().split(" ");
+                if (!values[0].equals(values[1])) {
+                    al.addEdge(Integer.parseInt(values[0]),
+                        Integer.parseInt(values[1]));
+                }
+            }
+            al.print();
+            break;
+        case "Matrix":
+            AdjacencyMatrix am = new AdjacencyMatrix(nodes, edges);
+            for (int i = 0; i < edges; i++) {
+                String[] values = sc.nextLine().split(" ");
+                if (!values[0].equals(values[1])) {
+                    am.addEdge(Integer.parseInt(values[0]),
+                        Integer.parseInt(values[1]));
+                }
+            }
+            am.print();
+            break;
+        default:
+            break;
+        }
+    }
 }
