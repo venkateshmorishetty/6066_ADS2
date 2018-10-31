3d6b351ddba1d49684d8ab7d95764b86a87023e1
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-31 21:30:15 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/3.1 Directed Cycle/Solution.java
+++ m3/3.1 Directed Cycle/Solution.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	public Solution() {
-		//constructor
-	}
-	/**
-	 * { main function }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int nodes = Integer.parseInt(sc.nextLine());
-		int edges = Integer.parseInt(sc.nextLine());
-		Digraph d = new Digraph(nodes);
-		for (int i = 0; i < edges; i++) {
-			int a = sc.nextInt(), b = sc.nextInt();
-			d.addEdge(a, b);
-		}
-		DirectedCycle dc = new DirectedCycle(d);
-		if(dc.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+        //constructor
+    }
+    /**.
+     * { main function }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nodes = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        Digraph d = new Digraph(nodes);
+        for (int i = 0; i < edges; i++) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            d.addEdge(a, b);
+        }
+        DirectedCycle dc = new DirectedCycle(d);
+        if (dc.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
