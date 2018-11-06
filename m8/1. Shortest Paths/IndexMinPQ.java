685e83c1b870446f0eb5d9dcf003d5b729c1a93f
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-11-06 18:16:07 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m8/1. Shortest Paths/IndexMinPQ.java
+++ m8/1. Shortest Paths/IndexMinPQ.java
@@ -1,14 +1,37 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
-public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
+/**
+ * Class for index minimum pq.
+ *
+ * @param      <Key>  The key
+ */
+public class IndexMinPQ<Key extends Comparable<Key>> {
+    /**.
+     * { maxN }
+     */
     private int maxN;
+    /**.
+     * { n }
+     */
     private int n;
+    /**.
+     * { pq }
+     */
     private int[] pq;
+    /**.
+     * { qp }
+     */
     private int[] qp;
+    /**.
+     * { keys }
+     */
     private Key[] keys;
-
-    public IndexMinPQ(int maxN) {
+    /**
+     * Constructs the object.
+     *
+     * @param      maxN  The maximum n
+     */
+    public IndexMinPQ(final int maxN) {
         if (maxN < 0) throw new IllegalArgumentException();
         this.maxN = maxN;
         n = 0;
@@ -18,21 +41,39 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
         for (int i = 0; i <= maxN; i++)
             qp[i] = -1;
     }
-
-
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
     public boolean isEmpty() {
         return n == 0;
     }
-
-    public boolean contains(int i) {
+    /**.
+     * { contains }
+     *
+     * @param      i     { i }
+     *
+     * @return     { true if it is, else false }
+     */
+    public boolean contains(final int i) {
         if (i < 0 || i >= maxN) throw new IllegalArgumentException();
         return qp[i] != -1;
     }
-
+    /**.
+     * { size }
+     *
+     * @return     { size }
+     */
     public int size() {
         return n;
     }
-
+    /**.
+     * { insert }
+     *
+     * @param      i     { i }
+     * @param      key   The key
+     */
     public void insert(int i, Key key) {
         if (i < 0 || i >= maxN) throw new IllegalArgumentException();
         if (contains(i)) throw new IllegalArgumentException("index is already in the priority queue");
@@ -42,122 +83,81 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
         keys[i] = key;
         swim(n);
     }
-
-    public int minIndex() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
-        return pq[1];
-    }
-
-
-    public Key minKey() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
-        return keys[pq[1]];
-    }
-
+    /**.
+     * { del min }
+     *
+     * @return     { min item }
+     */
     public int delMin() {
         if (n == 0) throw new NoSuchElementException("Priority queue underflow");
         int min = pq[1];
         exch(1, n--);
         sink(1);
         assert min == pq[n+1];
-        qp[min] = -1;        // delete
-        keys[min] = null;    // to help with garbage collection
-        pq[n+1] = -1;        // not needed
+        qp[min] = -1;
+        keys[min] = null;
+        pq[n+1] = -1;
         return min;
     }
-
-    public Key keyOf(int i) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        else return keys[i];
-    }
-
-    public void changeKey(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        keys[i] = key;
-        swim(qp[i]);
-        sink(qp[i]);
-    }
-
-    @Deprecated
-    public void change(int i, Key key) {
-        changeKey(i, key);
-    }
-    public void decreaseKey(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        if (keys[i].compareTo(key) <= 0)
-            throw new IllegalArgumentException("Calling decreaseKey() with given argument would not strictly decrease the key");
+    /**.
+     * { decrease key }
+     *
+     * @param      i     { i }
+     * @param      key   The key
+     */
+    public void decreaseKey(final int i, final Key key) {
         keys[i] = key;
         swim(qp[i]);
     }
-    public void increaseKey(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        if (keys[i].compareTo(key) >= 0)
-            throw new IllegalArgumentException("Calling increaseKey() with given argument would not strictly increase the key");
-        keys[i] = key;
-        sink(qp[i]);
-    }
-    public void delete(int i) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        int index = qp[i];
-        exch(index, n--);
-        swim(index);
-        sink(index);
-        keys[i] = null;
-        qp[i] = -1;
-    }
-    private boolean greater(int i, int j) {
+    /**.
+     * { grater }
+     *
+     * @param      i     { i }
+     * @param      j     { j }
+     *
+     * @return     { true if ,else false }
+     */
+    private boolean greater(final int i, final int j) {
         return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
     }
-
-    private void exch(int i, int j) {
+    /**.
+     * { exchanges }
+     *
+     * @param      i     { i }
+     * @param      j     { j }
+     */
+    private void exch(final int i, final int j) {
         int swap = pq[i];
         pq[i] = pq[j];
         pq[j] = swap;
         qp[pq[i]] = i;
         qp[pq[j]] = j;
     }
-
-    private void swim(int k) {
+    /**.
+     * { swim }
+     *
+     * @param      k     { k }
+     */
+    private void swim(final int temp) {
+        int k = temp;
         while (k > 1 && greater(k/2, k)) {
             exch(k, k/2);
             k = k/2;
         }
     }
-
-    private void sink(int k) {
+    /**.
+     * { sink }
+     *
+     * @param      k     { k }
+     */
+    private void sink(final int temp) {
+        int k = temp;
         while (2*k <= n) {
             int j = 2*k;
             if (j < n && greater(j, j+1)) j++;
             if (!greater(k, j)) break;
             exch(k, j);
             k = j;
-        }
-    }
-    public Iterator<Integer> iterator() { return new HeapIterator(); }
-
-    private class HeapIterator implements Iterator<Integer> {
-        // create a new pq
-        private IndexMinPQ<Key> copy;
-
-        // add all elements to copy of heap
-        // takes linear time since already in heap order so no keys move
-        public HeapIterator() {
-            copy = new IndexMinPQ<Key>(pq.length - 1);
-            for (int i = 1; i <= n; i++)
-                copy.insert(pq[i], keys[pq[i]]);
-        }
-
-        public boolean hasNext()  { return !copy.isEmpty();                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Integer next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            return copy.delMin();
         }
     }
 }
